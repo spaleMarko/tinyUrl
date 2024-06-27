427efac0c1c1a88bac6f125bc1a4f0659ba37e35
spalemarko <spasicmarko2010@hotmail.com>
2024-06-27 15:40:44 +0200

    Declare routes for server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+
+// Routes
+import urlRoutes from './routes/urlRoutes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', urlRoutes);
 
 app.listen(5000, () => {
   console.log(`Server running on port 5000`);
